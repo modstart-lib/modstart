5ad528986bd5db7ccc8de4d646d9a65bc320e51b
anonymous <anonymous@anonymous.com>
2022-02-09 09:08:40 +0800

    dev

--- src/helpers.php
+++ src/helpers.php
@@ -224,3 +224,18 @@ function L($name, ...$params)
     }
     return $name;
 }
+
+if (!function_exists('array_build')) {
+    function array_build($array, callable $callback)
+    {
+        $results = [];
+
+        foreach ($array as $key => $value) {
+            list($innerKey, $innerValue) = call_user_func($callback, $key, $value);
+
+            $results[$innerKey] = $innerValue;
+        }
+
+        return $results;
+    }
+}
