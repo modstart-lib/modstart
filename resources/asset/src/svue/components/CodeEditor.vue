c5f5b0fa15a615f92a43ce9f41f6cfe9ab7146a6
ModStart <modstart@163.com>
2023-06-27 09:45:59 +0800

    feat: code editor vue components

---
+++ resources/asset/src/svue/components/CodeEditor.vue
@@ -0,0 +1,68 @@
+<template>
+    <div>
+        <div :id="id" style="width:100%;height:100px;">{{ currentData }}</div>
+    </div>
+</template>
+
+<script>
+import {FieldVModel} from "../lib/fields-config";
+
+export default {
+    name: "CodeEditor",
+    mixins: [FieldVModel],
+    props: {
+        mode: {
+            type: String,
+            // 目前支持 json, css
+            default: 'json',
+        },
+    },
+    data() {
+        return {
+            id: 'CodeEditor' + Math.random().toString(36).substr(2),
+            editor: null,
+        }
+    },
+    watch: {
+        mode() {
+            this.updateMode()
+        }
+    },
+    mounted() {
+        MS.util.loadScript(this.$url.cdn('asset/vendor/ace/ace.js'), () => {
+            this.init()
+        })
+    },
+    methods: {
+        init() {
+            if (!window.ace) {
+                setTimeout(() => {
+                    this.init()
+                }, 100)
+                return
+            }
+            const editor = window.ace.edit(this.id);
+            editor.setTheme("ace/theme/monokai");
+            editor.session.on('change', () => {
+                // console.log('CodeEditor.changed', editor.session.getValue());
+                this.currentData = editor.session.getValue();
+            })
+            this.editor = editor
+            this.updateMode()
+        },
+        updateMode() {
+            if (!this.editor) {
+                setTimeout(() => {
+                    this.updateMode()
+                }, 100)
+                return
+            }
+            this.editor.session.setMode("ace/mode/" + this.mode);
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
