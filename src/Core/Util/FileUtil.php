a91bc4cbc06cc6b8ee56c6f9cb49f7bfb9d00ce0
ModStart <modstart@163.com>
2023-08-08 09:46:12 +0800

    feat: file util size format optimized

--- src/Core/Util/FileUtil.php
+++ src/Core/Util/FileUtil.php
@@ -360,7 +360,7 @@ class FileUtil
     {
         $size = sprintf("%u", $bytes);
         if ($size == 0) {
-            return ("0 Bytes");
+            return ("0B");
         }
         $units = ["B", "K", "M", "G", "T", "P", "E", "Z", "Y"];
         return round($size / pow(1024, ($i = floor(log($size, 1024)))), $decimals) . $units[$i];
