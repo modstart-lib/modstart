4b2a5e30c644bed693a18e9ec3da1fdf236d8d00
ModStart <modstart@163.com>
2023-10-26 13:33:51 +0800

    feat: JSON序列化使用统一封装处理

--- src/Core/Util/SerializeUtil.php
+++ src/Core/Util/SerializeUtil.php
@@ -6,6 +6,11 @@ namespace ModStart\Core\Util;
 
 class SerializeUtil
 {
+    /**
+     * @param $data
+     * @return false|string
+     * @deprecated delete at 2024-04-26
+     */
     public static function jsonObject($data)
     {
         if (empty($data)) {
@@ -14,12 +19,17 @@ class SerializeUtil
         return json_encode($data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
     }
 
-    public static function jsonEncode($data)
+    public static function jsonEncodeObject($data, $options = 0)
+    {
+        return json_encode($data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_FORCE_OBJECT);
+    }
+
+    public static function jsonEncode($data, $options = 0)
     {
         return json_encode($data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
     }
 
-    public static function jsonEncodePretty($data)
+    public static function jsonEncodePretty($data, $options = 0)
     {
         return json_encode($data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
     }
