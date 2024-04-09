451ed71f7ed625423cd1d8f647bfc608c4f11011
ModStart <modstart@163.com>
2024-04-09 10:12:58 +0800

    新增：Widget 相关操作优化

--- views/widget/chart.blade.php
+++ views/widget/chart.blade.php
@@ -2,7 +2,7 @@
 {!! \ModStart\ModStart::script("
 var ele = document.getElementById(".json_encode($id).");
 var chart = echarts.init(ele);
-chart.setOption(".$optionString.");
+chart.setOption(".(empty($optionString)?'{}':$optionString).");
 MS.ui.onResize( ele, chart.resize );
 $(ele).data('chart',chart);
 ") !!}
