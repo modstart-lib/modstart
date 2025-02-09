90b43407e04cbb07c68933a068b0b51b5e42fb70
ModStart <modstart@163.com>
2025-02-09 19:28:36 +0800

    新增：DataStorageProvider 支持，上传配置性能优化

---
+++ src/Data/DataStorageProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace ModStart\Data;
+
+use Illuminate\Support\Str;
+use ModStart\Core\Provider\ProviderTrait;
+
+/**
+ * @method static AbstractDataStorageProvider[] listAll()
+ */
+class DataStorageProvider
+{
+    use ProviderTrait;
+
+    static $list = [];
+
+    public static function defaultEnabledDriver()
+    {
+        foreach (self::listAll() as $driver) {
+            if ($driver->enable()) {
+                $name = $driver->name();
+                if (!Str::startsWith($name, 'Data')) {
+                    $name = 'Data' . $name;
+                }
+                return 'DataStorage_' . $name;
+            }
+        }
+        return null;
+    }
+}
