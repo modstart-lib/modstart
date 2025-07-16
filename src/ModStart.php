517dcf655f71a9d8e7379179a5e0510f0fbbf102
ModStart <modstart@163.com>
2025-07-16 18:00:55 +0800

    优化：后台管理更好的支持菜单自定义

--- src/ModStart.php
+++ src/ModStart.php
@@ -16,7 +16,7 @@ use ModStart\Support\Manager\WidgetManager;
  */
 class ModStart
 {
-    public static $version = '4.1.1';
+    public static $version = '4.1.2';
 
     public static $script = [];
     public static $style = [];
